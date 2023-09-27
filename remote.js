ecadf65bb9d71a87139f6d1b3a754272c0e6ea74
lake-straly <128569602+lake-straly@users.noreply.github.com>
2023-09-27 11:18:13 -0700

    Update remote.js

--- remote.js
+++ remote.js
@@ -1,1 +1,9 @@
-
+javascript:(function() {
+    var rawFileUrl = 'https://raw.githubusercontent.com/lake-straly/g5-integrations-note-taker/main/note-taker.js';
+    fetch(rawFileUrl)
+        .then(response => response.text())
+        .then(code => {
+            eval(code);
+        })
+        .catch(error => console.error('Error fetching code:', error));
+})();
